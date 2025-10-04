9578dcfee2db0c34226ffff6a5b35e763b728ef7
Nikita Chernov <rouming44life@gmail.com>
2025-10-04 14:53:53 +0300

    l2_12 minor changes are done

--- l2_12/main.go
+++ l2_12/main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func main() {
-	var (
-		c int
-	)
+type Flags struct {
+	c bool // flag to write amount of desired lines
+}
 
-	flag.IntVar(&c, "c", 1, "number of matches")
+func (f *Flags) getCParameter() bool {
+	return f.c
+}
+
+func grepFunc(substring string, sentences []string, flags Flags) []string {
+
+	for _, sentence := range sentences {
+		if strings.Contains(sentence, substring) {
+
+		}
+	}
+	return sentences
+}
+
+func main() {
+	// flag's parameters
+	var flags Flags
+	// sentences
+	var sentences []string
+
+	flag.BoolVar(&flags.c, "c", false, "number of matches")
 	flag.Parse()
 
 	// file path to read from
@@ -18,5 +42,45 @@ func main() {
 
 	if filePath != "" {
 		file, err := os.Open(filePath)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer func() {
+			if err := file.Close(); err != nil {
+				log.Fatalln(err)
+			}
+		}()
+		// Reading of file
+		readline := bufio.NewScanner(file)
+		for readline.Scan() {
+			sentences = append(sentences, readline.Text())
+		}
+	} else {
+		var (
+			amountOfSentences int
+			err               error
+		)
+		fmt.Println("Enter amount of sentences: ")
+		sc := bufio.NewScanner(os.Stdin)
+		// scan amount of sentences
+		for {
+			sc.Scan()
+			amountOfSentences, err = strconv.Atoi(sc.Text())
+			if err != nil {
+				log.Println("Error: ", err)
+				fmt.Println("Please enter a valid amount of sentences")
+				continue
+			}
+			break
+		}
+		sentences = make([]string, amountOfSentences)
+		for i := range sentences {
+			// fill sentence's array
+			sc.Scan()
+			sentences[i] = sc.Text()
+		}
 	}
+
+	// Call grep func
+	grepFunc(sentences, flags)
 }
