c2fed2f6b86eeb36c1a338512fee685d383b3bd3
Nikita Chernov <rouming44life@gmail.com>
2025-10-07 23:41:41 +0300

    l2_13 was done

--- l2_13/main.go
+++ l2_13/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
+	"log"
+	"os"
 	"sort"
 	"strconv"
 	"strings"
@@ -9,8 +13,8 @@ import (
 
 type flags struct {
 	f string
-	// d string
-	// s string
+	d string // new separator
+	s bool   //
 }
 
 func (f *flags) parseFParameter() ([]int, error) {
@@ -49,18 +53,87 @@ func (f *flags) parseFParameter() ([]int, error) {
 	return indexes, nil
 }
 
+func toFindStringFiedlsLikeSliceOfStrings(indexes []int, strs []string) []string {
+	if len(indexes) == 0 {
+		return strs
+	}
+	var necessaryStrings []string = make([]string, len(indexes))
+	stopInd := 0
+	for i, index := range indexes {
+		if index-1 >= len(strs) || index-1 < 0 {
+			stopInd = i
+			break
+		}
+		necessaryStrings[i] = strs[index-1] // to avoid an empty element, first index which program can get is 1 -> Though to substruct 1 from index value
+	}
+	return necessaryStrings[:stopInd]
+}
+
 func main() {
 	// flags struct
 	var flags flags
-	//var separator = "\t"
+	//var (
+	//	sentences           []string
+	//	resultCuttedStrings []string
+	//)
 	// parse flags
 	flag.StringVar(&flags.f, "f", "", "getting fields parameter")
+	flag.StringVar(&flags.d, "d", "\t", "use inputted separator")
+	flag.BoolVar(&flags.s, "s", false, "get parameter to ignore strings without separator")
 	flag.Parse()
 
-	//scanner := bufio.NewScanner(os.Stdin)
-	//for scanner.Scan() {
-	//	line := scanner.Text()
-	//	lines := strings.Split(line, separator)
-	//
-	//}
+	if flags.f == "" {
+		fmt.Println("you must specify a list fields")
+		return
+	}
+
+	filePath := flag.Arg(0)
+	// flag analyze:
+	separator := flags.d
+	isIgnoreStringsWithoutSeparator := flags.s
+	// fields numbers
+	fields, err := flags.parseFParameter()
+	if err != nil {
+		// also can get an empty array
+		fmt.Println(err)
+	}
+
+	if filePath == "" {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			line := scanner.Text()
+			lines := strings.Split(line, separator)
+			if len(lines) == 1 && isIgnoreStringsWithoutSeparator { // slice of strings is not changed
+				// string is ignored
+				continue
+			}
+			// Get necessary strings as slice of strings
+			strNecessaryFields := toFindStringFiedlsLikeSliceOfStrings(fields, lines)
+			result := strings.Join(strNecessaryFields, separator)
+			// cleaning from last separator
+			//result = result[:len(result)]
+			fmt.Println(result)
+		}
+	} else {
+		// read from file
+		file, err := os.OpenFile(filePath, os.O_RDONLY, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := scanner.Text()
+			lines := strings.Split(line, separator)
+			if len(lines) == 1 && isIgnoreStringsWithoutSeparator { // slice of strings is not changed
+				// string is ignored
+				continue
+			}
+			// Get necessary strings as slice of strings
+			strNecessaryFields := toFindStringFiedlsLikeSliceOfStrings(fields, lines)
+			result := strings.Join(strNecessaryFields, separator)
+			// cleaning from last separator
+			//result = result[:len(result)]
+			fmt.Println(result)
+		}
+	}
 }
