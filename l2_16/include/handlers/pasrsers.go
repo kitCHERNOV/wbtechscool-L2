151c6c8dca49928c342b46b2a45c73ec2a3e34c5
Nikita Chernov <rouming44life@gmail.com>
2025-10-31 00:55:35 +0300

    311025 changes

--- l2_16/include/handlers/pasrsers.go
+++ l2_16/include/handlers/pasrsers.go
@@ -4,7 +4,12 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"path/filepath"
 	"reflect"
+	"strings"
 	"wget/include/logger"
 
 	"golang.org/x/net/html"
@@ -35,22 +40,82 @@ func (incLinks *htmlIncludedLinks) DownloadPages() {
 	Logger := &incLinks.logger
 
 	v := reflect.ValueOf(incLinks).Elem()
-
+	t := v.Type()
 	// stringSliceType for checking fields
 	stringSliceType := reflect.TypeOf([]string{})
 	for i := 0; i < v.NumField(); i++ {
 		if v.Field(i).Type() != stringSliceType {
 			continue
 		}
-		for _, url := range v.Field(i).Interface().([]string) {
-			resp, err := http.Get(url)
-			if err != nil {
-				fmt.Fprintf(Logger, "getting url: %s error: %v", url, err)
-			}
+		// get description of elem
+		field := v.Field(i)
+		fieldName := t.Field(i).Name
 
-			field := v.Field(i)
-			fieldName := field.
-
+		var folderName string
+		switch fieldName {
+        case "CssLinks":
+            folderName = "css"
+        case "JsLinks":
+            folderName = "js"
+        case "Images":
+            folderName = "images"
+        default:
+            continue
+        }
+		// all directories shoud be created in main func
+		urls := field.Interface().([]string)
+		for _, url := range urls {
+			
+			
 		}
 	}
+}
+
+func downloadFile(urlStr, folder string, logger *logger.Logger) {
+	// get file name
+	parsedURL, err := url.Parse(urlStr)
+	if err != nil {
+		fmt.Fprintf(logger, "parse url error: %w", err)
+		return
+	}
+
+	fileName := path.Base(parsedURL.Path)
+	if fileName == "/" || fileName == "." {
+		// if file name is not determined, just generate its
+		fileName = generateFileName(urlStr, folder)
+	}
+
+	filePath := filepath.Join(folder, fileName)
+
+	// download file
+	resp, err := http.Get(urlStr)
+	if err != nil {
+		fmt.Fprintf(logger, "getting url: %s error: %v", urlStr, err)
+	}
+
+	defer resp.Body.Close()
+
+	// Check download status
+	if resp.StatusCode != http.StatusOK {
+		
+	}
+}
+
+
+func generateFileName(urlStr, folder string) string {
+    // Используем хеш URL или timestamp
+    hash := fmt.Sprintf("%d", len(urlStr)) // Простой пример
+    
+    var ext string
+    switch folder {
+    case "css":
+        ext = ".css"
+    case "js":
+        ext = ".js"
+    case "images":
+        // Можно попробовать определить из Content-Type
+        ext = ".png" // По умолчанию
+    }
+    
+    return fmt.Sprintf("file_%s%s", hash, ext)
 }
